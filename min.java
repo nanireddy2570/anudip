4aa5238acf6859047705b0db80390aa24e53b2e4
nanireddy2570 <nanireddy2570@gmail.com>
2022-10-26 16:46:21 +0530

    file

---
+++ min.java
@@ -0,0 +1,26 @@
+class test {
+static void main ( int a[]){
+int min=a[0];
+for(int i=1;i<a.length;i++)
+if(min>a[i])
+min=a[i];
+system.out.println("minimum value in array:"+min);
+}
+static void even or odd(inta[]){
+int temp=a[0];
+for(int i=1;i<a.length;i++)
+if(temp%2==0)
+system.out.println("even nos:"+temp);
+system.out.println("odd nos:"+temp);
+}
+else
+{
+public static void main(string nani[])
+{
+int a[]={1,2,3,4,5,6,7,8,9,10}
+min (a);
+even(a);
+odd(a);
+}
+}
+}
